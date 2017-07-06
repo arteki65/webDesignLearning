5f463cdea047ecf8f107279733924c7d1ce55ad9
arteki65 <arteki65@gmail.com>
2017-07-06 20:30:21 +0200

    NoCash improve mobile nav

--- noCashDesign/resources/js/navigation.js
+++ noCashDesign/resources/js/navigation.js
@@ -1,7 +1,11 @@
 var animationDuration = 300;
 var mobileNavLeftMargin;
+var windowWidth;
+var navOpen = false;
 
 $(document).ready(function() {
+    windowWidth = $(window).width();
+    
     if($(window).width() < 481) {
         mobileNavLeftMargin = 0;
     } else {
@@ -9,6 +13,10 @@ $(document).ready(function() {
     }
     
     $(window).resize(function() {
+        if(windowWidth === $(window).width()) {
+            return;
+        }
+        
         if($(window).width() < 481) {
             mobileNavLeftMargin = 0;
         } else {
@@ -17,17 +25,35 @@ $(document).ready(function() {
         
         if($(window).width() > 767) {
             $('nav').css("left", "0");
+            if(navOpen) {
+                $('#body-mask').hide();
+            }
         } else {
-            $('nav').css("left", "100%");
+            if(!navOpen) {
+                $('nav').css("left", "100%");
+            } else {
+                $('nav').css("left", mobileNavLeftMargin + "%");
+                $('#body-mask').css("right", 100 - mobileNavLeftMargin + "%");
+                $('#body-mask').show();
+            }
         }
+        
+        windowWidth = $(window).width();
     });
     
-    $('.js--open-mobile-nav-icon').click(function() {
+    $('.js--open-mobile-nav-icon').click(function() {        
         $('.mobile-current-nav').animate({
             "margin-top": "-100px"
         }, animationDuration);
         $('nav').animate({
             "left": mobileNavLeftMargin + "%"
+        }, animationDuration);
+        
+        navOpen = true;
+        
+        $('#body-mask').show();
+        $('#body-mask').animate({
+            right: 100 - mobileNavLeftMargin + "%"
         }, animationDuration);
     });
     
@@ -38,5 +64,9 @@ $(document).ready(function() {
         $('.mobile-current-nav').animate({
             "margin-top": 0
         }, animationDuration);
+        
+        navOpen = false;
+        $('#body-mask').hide();
+        $('#body-mask').css("right", 0);
     });
 });
