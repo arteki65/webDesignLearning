eeebec8163fe71f6c59ef239dfb4d9cebc94416f
Arkadiusz Aptewicz <Arkadiusz.Aptewicz@dbschenker.com>
2018-01-09 16:45:15 +0100

    prepare for output management

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,20 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Logo from './tmt_logo_small.png';
 
 function component() {
 
     var element = document.createElement('div');
 
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    
-    // Add the image to our existing div
-    var logo = new Image();
-    logo.src = Logo;
-    
-    element.appendChild(logo);
 
     return element;
 }
